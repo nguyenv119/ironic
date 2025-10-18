4c40e784e178c34b2df229d392f9bdad44914301
Long Nguyen <nguyenv@brandeis.edu>
2025-10-18 01:35:29 -0400

    cicd: added github actions for pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 const nextConfig = {
-  experimental: {
-    turbopack: {
-      root: __dirname,
-    },
+  basePath: "/ironic",
+  assetPrefix: "/ironic/",
+  images: { unoptimized: true },
+  output: "export",
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  turbopack: {
+    root: __dirname,
   },
 };
 
